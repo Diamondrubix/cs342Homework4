75d38d93e72250453d5dc88f19901267f28b5633
zzahra2 <zainz23@github.com>
2018-11-14 14:09:06 -0600

    NEW PLACE: LavaRoom.java (all new places inherit from Place.java)

---
+++ Game4/src/place/LavaRoom.java
@@ -0,0 +1,16 @@
+// Name: Zain Zahran
+// Netid: zzahra2
+
+package place;
+
+import artifact.Artifact;
+import game.CleanLineScanner;
+
+// The LavaRoom inherits from a normal place
+// Required: LavaSuit in order to pass through this place
+public class LavaRoom extends Place {
+	@Override
+	public void ambientFunction() {
+		return;
+	}
+}
