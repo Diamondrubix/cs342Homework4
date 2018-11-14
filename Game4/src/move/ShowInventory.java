b165a8279595f3d49c0838d05ab1cf43a0383310
Alex Oey <>
2018-11-14 15:01:33 -0600

    fix

---
+++ Game4/src/move/ShowInventory.java
@@ -0,0 +1,23 @@
+/* Author: Alexander Oey (NetID: aoey2) */
+package move;
+
+import character.Character;
+import place.Place;
+import move.Move;
+
+/**
+ * This class encapsulates a command to redisplay the information about current place.
+ *
+ * @author Alexander Oey (aoey2)
+ */ 
+public class ShowInventory extends Move {
+	private final Character character;
+	
+	public ShowInventory(Character c) {
+		this.character = c;
+	}
+	
+	public void execute() {
+		c.displayInventory();
+	}
+}
