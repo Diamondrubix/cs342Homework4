b69704294bc38f8d85621739d7845b5a5e309acf
zzahra2 <zainz23@github.com>
2018-11-14 14:39:09 -0600

    NEW PLACE: UnderwaterRoom, Overrided ambientFunction() and added description

---
+++ Game4/src/place/UnderwaterRoom.java
@@ -0,0 +1,28 @@
+// Name: Zain Zahran
+// Netid: zzahra2
+
+package place;
+
+import artifact.Artifact;
+import game.CleanLineScanner;
+
+/* UNDERWATER ROOM */
+// Desc: This room is completely submerged in cold water. 
+//		Player/NPC should not stay here too long unless they have a ScubaSuit.
+// AmbientFunction: User is required to have a ScubaSuit in their
+// 		inventory. Otherwise, -10 hp per turn.
+// Extra-Idea: Should ScubaSuit regen hp when user swims in this place?
+
+public class Underwater extends Place {
+	@Override
+	public void ambientFunction(Character c) {
+		// Check Player/NPC inventory for a ScubaSuit
+		if (c.checkFor("ScubaSuit") ) {
+			// Do nothing OR regen HP
+			return;
+		}
+		else {
+			c.damage(10);	// Take off 10 hp per turn
+		}
+	}
+}
