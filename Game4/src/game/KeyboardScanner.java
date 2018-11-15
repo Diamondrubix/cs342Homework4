dc70f4b43d9da9cccc25a22c19003382143a096c
Alex Oey <>
2018-11-15 02:00:51 -0600

    Completed javadoc for game package

--- Game4/src/game/KeyboardScanner.java
+++ Game4/src/game/KeyboardScanner.java
@@ -1,4 +1,8 @@
-/* Author: Alexander Oey (NetID: aoey2) */
+/* 
+ * Author: 
+ * Alexander Oey (aoey2) 
+ * Adam Arato (aarato2)
+ */
 package game;
 
 import Network.Client;
@@ -11,6 +15,7 @@ import java.util.Scanner;
  * The purpose of this class is to maintain single Scanner object to System.in.
  *
  * @author Alexander Oey (aoey2)
+ * @author Adam Arato (aarato2)
  */
 public class KeyboardScanner{
 	private Scanner keyboard;
@@ -20,7 +25,7 @@ public class KeyboardScanner{
 	/**
 	 * Returns the Scanner object to System.in
 	 * 
-	 * @return Scanner object
+	 * @return KeyboardScanner instance
 	 */
 	public static KeyboardScanner getKeyboardScanner() {
 		//First use of KeyboardScanner
@@ -30,7 +35,12 @@ public class KeyboardScanner{
 		return kScanner;
 	}
 
-
+	/**
+	 * Returns a string input from keyboard delimited by a new line character.
+	 * This method also broadcasts the input to all clients over the network.
+	 *
+	 * @return string input
+	 */
 	public String nextLine(){
 		String line = keyboard.nextLine();
 
@@ -42,6 +52,12 @@ public class KeyboardScanner{
 		return line;
 	}
 
+	/**
+	 * Returns an integer input from keyboard delimited by a whitespace.
+	 * This method also broadcasts the input to all clients over the network.
+	 *
+	 * @return integer input
+	 */
 	public int nextInt(){
 		int l = keyboard.nextInt();
 
