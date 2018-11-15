48dd95b45d1578687879b1559dded5830d3020d3
zzahra2 <zainz23@github.com>
2018-11-14 20:20:18 -0600

    NEW PLACE: FountainRoom - Ambient function overrided, Player gets healed while in this room

---
+++ Game4/src/place/FountainRoom.java
@@ -0,0 +1,20 @@
+// Name: Zain Zahran
+// Netid: zzahra2
+
+package place;
+
+import artifact.Artifact;
+import game.CleanLineScanner;
+
+/* FOUNTAIN ROOM */
+// Desc: This room is very colorful and bright. The light shines
+//		upon a crystal clear water fountain. The user heals while standing
+//		nearby it.
+// AmbientFunction: User gets healed 10hp a turn while staying in this room.
+
+public class FountainRoom extends Place {
+	@Override
+	public void ambientFunction(Character c) {
+		c.heal(10);	// Heal 10 hp per turn in this room
+	}
+}
